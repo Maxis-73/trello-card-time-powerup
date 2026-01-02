24578f7b8720ad343e53bcabb732ed4f2cdc2f14
Ernesto Armenta <ernesto_armenta_2000@hotmail.com>
2026-01-02 11:54:56 -0700

    Testing dynamic badge

--- js/connector.js
+++ js/connector.js
@@ -1,25 +1,38 @@
 import { utils } from "./utils.js";
 
 window.TrelloPowerUp.initialize({
-    'card-badges': function (t, options) {
-        return t.card('id')
-            .then(function (card) {
-                const boardTime = utils.getRelativeTime(utils.getDateFromCardId(card.id));
-                return [{
-                    text: boardTime,
-                    refresh: 60,
-                }]
-            })
+    "card-badges": function (t, opts) {
+        let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+        return t
+            .card("name")
+            .get("name")
+            .then(function (cardName) {
+                console.log("We just loaded the card name for fun: " + cardName);
+                return [
+                    {
+                        // Dynamic badges can have their function rerun
+                        // after a set number of seconds defined by refresh.
+                        // Minimum of 10 seconds.
+                        dynamic: function () {
+                            // we could also return a Promise that resolves to
+                            // this as well if we needed to do something async first
+                            return {
+                                text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
+                                icon: "./images/icon.svg",
+                                color: "green",
+                                refresh: 10, // in seconds
+                            };
+                        },
+                    },
+                    {
+                        // It's best to use static badges unless you need your
+                        // badges to refresh.
+                        // You can mix and match between static and dynamic
+                        text: "Static",
+                        icon: HYPERDEV_ICON, // for card front badges only
+                        color: null,
+                    },
+                ];
+            });
     },
-    'card-detail-badges': function (t, options) {
-        return t.card('id')
-            .then(function (card) {
-                const boardTime = utils.getRelativeTime(utils.getDateFromCardId(card.id));
-                return [{
-                    title: 'Tiempo en tablero',
-                    text: boardTime,
-                    refresh: 60,
-                }]
-            })
-    }
-})
+});
