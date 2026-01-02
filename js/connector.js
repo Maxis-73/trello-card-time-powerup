18345dc53320c61d99a7b63b7ce997439b4204e2
Ernesto Armenta <ernesto_armenta_2000@hotmail.com>
2026-01-02 12:23:36 -0700

    prueba de icono

--- js/connector.js
+++ js/connector.js
@@ -13,7 +13,7 @@ window.TrelloPowerUp.initialize({
                             const relativeTime = utils.getRelativeTime(creationDate);
                             return {
                                 text: relativeTime,
-                                icon: "../icons/time.svg",
+                                icon: "./icons/time.svg",
                                 refresh: 60,
                             };
                         },
